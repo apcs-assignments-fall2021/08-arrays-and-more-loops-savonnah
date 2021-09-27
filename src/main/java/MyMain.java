ac95984600e30fa61ab2aca5ed1a5927a7f7b3ba
savonnahwong <23swong@athenian.org>
2021-09-26 22:16:48 -0700

    not finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,15 +5,33 @@ public class MyMain {
     // Rotates all values in an array to the left
     // Ex. rotateLeft([1, 2, 3, 4, 5]) => [2, 3, 4, 5, 1]
     public static int[] rotateLeft(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int temp[] = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++)
+            temp[i] = arr[i];
+
+        arr[arr.length-1] = arr[0];
+
+        for (int i = 0; i < arr.length-1; i++) {
+            arr[i] = temp[i+1];
+        }
+        return arr;
     }
 
     // Rotates all values in an array to the right
     // Ex. rotateRight([1, 2, 3, 4, 5]) => [5, 1, 2, 3, 4]
     public static int[] rotateRight(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int temp[] = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++)
+            temp[i] = arr[i];
+
+        arr[0] = arr[arr.length-1];
+
+        for (int i = 0; i < arr.length-1; i++) {
+            arr[i + 1] = temp[i];
+        }
+        return arr;
     }
 
     // Replaces all duplicate values with -1
@@ -25,6 +43,8 @@ public class MyMain {
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        System.out.println(Arrays.toString(rotateLeft(new int[] {1, 2, 3, 4, 5})));
+        System.out.println(Arrays.toString(rotateRight(new int[] {1, 2, 3, 4, 5})));
+        System.out.println(Arrays.toString(duplicateReplacer(new int[] {1, 2, 1, 2, 1, 1})));
     }
 }
